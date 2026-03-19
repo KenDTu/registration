64d4cd71712c227cf03dcab6ec4e817484202eef
Ken Tu <kentu@Kens-Huge-MacBook-Pro.local>
2026-03-18 23:36:11 -0700

    Added JSON file layout

--- logicCourseReg.py
+++ logicCourseReg.py
@@ -2,3 +2,7 @@
 The logicCourseReg.py module provides the algorithm to compute all possible course schedules with your given
 constraints.
 """
+
+import json # JavaScript Object Notation - data stored as an object 
+
+# Next steps to load the possibleClasses JSON file into this logic file as a data to be used to create individual classes.
